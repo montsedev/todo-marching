122222e762f4fbfb29dabb1992ff0bafd16c782c
MontseDev <montsevm.03@gmail.com>
2023-05-14 19:39:48 -0600

    Finished App

--- src/App/index.js
+++ src/App/index.js
@@ -1,32 +1,16 @@
-//** dependence **//
-import { useState } from 'react';
+import React from 'react';
+import { AppUI } from './AppUI';
 
-//** components **//
-import AppUI from './AppUI';
-
-//const defaultTodos = [
-//  { text: 'Cortar cebolla', completed: false },
-//  { text: 'Tomar curso introducción a react', completed: false },
-//  { text: 'Llorar con la llorona', completed: true },
-//]
+const defaultTodos = [
+  { text: 'Cortar cebolla', completed: true },
+  { text: 'Tomar el cursso de intro a React', completed: false },
+  { text: 'Llorar con la llorona', completed: true },
+  { text: 'LALALALAA', completed: false },
+];
 
 function App() {
-    // Traemos nuestros TODOs almacenados
-  const localStorageTodos = localStorage.getItem('TODOS_V1');
-  let parsedTodos;
-
-  if (!localStorageTodos) {
-    // Si el usuario es nuevo no existe un item en localStorage, por lo tanto guardamos uno con un array vacío
-    localStorage.setItem('TODOS_V1', JSON.stringify([]));
-    parsedTodos = '';
-  } else {
-    // Si existen TODOs en el localStorage los regresamos como nuestros todos
-    parsedTodos = JSON.parse(localStorageTodos);
-  }
-
-  // Guardamos nuestros TODOs del localStorage en nuestro estado
-  const [todos, setTodos] = useState(parsedTodos);
-  const [searchValue, setSearchValue] = useState('');
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
@@ -56,7 +40,7 @@ function App() {
     newTodos.splice(todoIndex, 1);
     setTodos(newTodos);
   };
-  
+
   return (
     <AppUI
       totalTodos={totalTodos}
